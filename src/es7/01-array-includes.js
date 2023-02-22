8d6d8c98d1e5fe1fd191bed98155aeefadc84dcd
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-02-21 19:03:11 -0600

    ES7

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,8 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+console.log(numbers.includes(4)) // true
+console.log(numbers.includes(445)) // false
+
+let names = ['mike', 'oscar', 'DAVID']
+console.log(names.includes('david')) // false
+
